2065bee0c5df71f61e2970be2c5e037dda4f6a67
Robert Vasquez Zavaleta <rvasquez@robotnik.es>
2022-07-13 10:23:12 +0200

    rb1_base_sim: Add rviz

--- rb1_base_sim/rb1_base_gazebo/launch/gazebo_rviz.launch.py
+++ rb1_base_sim/rb1_base_gazebo/launch/gazebo_rviz.launch.py
@@ -5,15 +5,17 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-# from ament_index_python.packages import get_package_prefix
 
 def generate_launch_description():
 
     pkg_rb1_base_gazebo = get_package_share_directory('rb1_base_gazebo')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    rviz_file = os.path.join(pkg_rb1_base_gazebo, 'rviz', 'rb1_base.rviz')
 
     gazebo_world = os.path.join(
         pkg_rb1_base_gazebo,
@@ -31,6 +33,15 @@ def generate_launch_description():
     )    
 
 
+    start_rviz = Node(
+        package='rviz2',
+        namespace='',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d' + rviz_file]
+    )
+    print(rviz_file)
     return LaunchDescription([
-        start_gazebo
+        start_gazebo,
+        start_rviz
     ])
